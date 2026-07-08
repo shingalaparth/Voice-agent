1454739b26bfd0f19f20576753308816fe085298
Parth Shingala <Parthshingala6969@gmail.com>
2026-07-08 13:06:14 +0530

    u[date

--- dashboard/src/app/(app)/layout.tsx
+++
@@ -1,9 +0,0 @@
-import { AppShell } from "@/components/shell/app-shell";
-
-export default function AppLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return <AppShell>{children}</AppShell>;
-}
